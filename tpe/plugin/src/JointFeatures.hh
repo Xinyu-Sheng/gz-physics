8f1d923376e5263f69ff1766728c62fa0552b5c5
Xinyu Sheng <sheng.xin.yu@foxmail.com>
2025-12-30 08:32:58 +0800

    Simplify TPE joint API for minimal DetachableJoint support
    - Remove 18+ stub method implementations from JointFeatures
    - Keep only core DetachableJoint methods: AttachFixedJoint, DetachJoint, SetJointTransformFromParent
    - Reduce joint code from ~180 lines to ~50 lines
    
    Complements gz-sim decoupling changes.
    
    Signed-off-by: Xinyu Sheng <sheng.xin.yu@foxmail.com>

--- tpe/plugin/src/JointFeatures.hh
+++ tpe/plugin/src/JointFeatures.hh
@@ -29,8 +29,7 @@ namespace physics {
 namespace tpeplugin {
 
 struct JointFeatureList : FeatureList<
-  GetJointFromModel, GetBasicJointProperties, GetBasicJointState,
-  SetBasicJointState, SetJointTransformFromParentFeature,
+  SetJointTransformFromParentFeature,
   DetachJointFeature, AttachFixedJointFeature
 > { };
 
@@ -38,28 +37,6 @@ class JointFeatures :
     public virtual Base,
     public virtual Implements3d<JointFeatureList>
 {
-  public: std::size_t GetJointCount(const Identity &_modelID) const override;
-  public: Identity GetJoint(const Identity &_modelID, std::size_t _jointIndex) const override;
-  public: Identity GetJoint(const Identity &_modelID, const std::string &_jointName) const override;
-  public: const std::string &GetJointName(const Identity &_jointID) const override;
-  public: std::size_t GetJointIndex(const Identity &_jointID) const override;
-  public: Identity GetModelOfJoint(const Identity &_jointID) const override;
-
-  public: std::size_t GetJointDegreesOfFreedom(const Identity &_id) const override;
-  public: Pose3d GetJointTransformFromParent(const Identity &_id) const override;
-  public: Pose3d GetJointTransformToChild(const Identity &_id) const override;
-
-  public: double GetJointPosition(const Identity &_id, std::size_t _dof) const override;
-  public: double GetJointVelocity(const Identity &_id, std::size_t _dof) const override;
-  public: double GetJointAcceleration(const Identity &_id, std::size_t _dof) const override;
-  public: double GetJointForce(const Identity &_id, std::size_t _dof) const override;
-  public: Pose3d GetJointTransform(const Identity &_id) const override;
-
-  public: void SetJointPosition(const Identity &_id, std::size_t _dof, double _value) override;
-  public: void SetJointVelocity(const Identity &_id, std::size_t _dof, double _value) override;
-  public: void SetJointAcceleration(const Identity &_id, std::size_t _dof, double _value) override;
-  public: void SetJointForce(const Identity &_id, std::size_t _dof, double _value) override;
-
   public: void SetJointTransformFromParent(const Identity &_id, const Pose3d &_pose) override;
   public: void DetachJoint(const Identity &_jointId) override;
   public: Identity CastToFixedJoint(const Identity &_jointID) const override;
